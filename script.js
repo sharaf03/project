5eadab746f2d38b9208a97cb622ce5009c0440cf
Sharaf <sharafjomkholmahmadov@gmail.com>
2021-07-17 21:14:27 +0300

    First commit

--- script.js
+++ script.js
@@ -10,9 +10,8 @@ let incr = 10,
 console.log(++incr);
 console.log(--decr);
 console.log(5%2);
-console.log(2+2*2 ===8);
-
-const isChecked = true;
+console.log(2+2*2 ===8);const isChecked = true;
 const isClose = true;
 console.log(isChecked || isClose);
+
 
